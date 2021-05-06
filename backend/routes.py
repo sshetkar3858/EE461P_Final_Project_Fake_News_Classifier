442b193ba5f28c9692923068d9ab9c7508e708a4
NADelValle <nicolasdelvalle@utexas.edu>
2021-05-05 19:30:48 -0500

    added flask backend

---
+++ backend/routes.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import time
+
+# a simple page that says hello
+@app.route('/test')
+def hello():
+    return {'Hello, World!': "What's good. What's REALLY good?", 'time': time.time()}
+
+@app.route('/')
+def home():
+    return 'News: Fake or Factual?'
